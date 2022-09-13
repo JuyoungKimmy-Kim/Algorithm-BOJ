be64e845768d2f71a5b245f2e9d0e5e37f9193ee
Juyoung Kim <kimmykim.jy@gmail.com>
2022-09-13 23:31:27 +0900

    220913 commit

--- BOJ/src/dynamicprogramming/BOJ1103.java
+++ BOJ/src/dynamicprogramming/BOJ1103.java
@@ -3,7 +3,10 @@ package dynamicprogramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ1103 {
@@ -12,36 +15,75 @@ public class BOJ1103 {
 	static final int dx[]= {1,-1,0,0};
 	
 	static int N, M;
-	static int [][] map;
-	static int [][]dp;
-	static boolean[][] visited;
+	static char [][] map;
+	static int[][] dp;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader (new InputStreamReader (System.in));
 		StringTokenizer st=new StringTokenizer (br.readLine());
-		
+
 		N=Integer.parseInt(st.nextToken());
 		M=Integer.parseInt(st.nextToken());
 		
-		map=new int[N][M];
+		dp=new int[N][M];
 		for (int i=0; i<N; i++)
 			Arrays.fill(dp[i], -1);
-		visited=new boolean[N][N];
+		
+
+		map=new char[N][];
 		
 		for (int i=0; i<N; i++) {
-			st=new StringTokenizer (br.readLine());
+			String line=br.readLine();
+			map[i]=line.toCharArray();
+		}
+		System.out.println(bfs());
+		
+	}
+//	private static int bfs () {
+//		Queue<int[] >q=new ArrayDeque<> ();
+//		q.add(new int[] {0,0});
+//		dp[0][0]=1;
+//		
+//		int cnt=1;
+//		while (!q.isEmpty()) {
+//			int y=q.peek()[0];
+//			int x=q.poll()[1];
+//			int s=map[y][x]-'0';
+//			
+//			boolean flag=false;
+//			for (int d=0; d<4; d++) {
+//				int ny=y+dy[d]*s;
+//				int nx=x+dx[d]*s;
+//				
+//				if (ny<0 || nx<0 || ny>=N || nx>=M || map[ny][nx]=='H') continue;
+//				
+//				if (dp[ny][nx]!=-1 && dp[ny][nx]-'0'<=s)
+//					flag=true;
+//				
+//				if (dp[ny][nx]==-1) {
+//					dp[ny][nx]=dp[y][x]+1;
+//					q.add(new int[] {ny, nx});
+//					cnt=dp[ny][nx];
+//				}
+//			}
+//			if (flag) return -1;
+//		}
+//		
+//		print();
+//		return cnt;
+//	}
+	
+
+
+	private static void print () {
+		System.out.println("===============================");
+		for (int i=0; i<N; i++) {
 			for (int j=0; j<M; j++) {
-				map[i][j]=Integer.parseInt(st.nextToken());
-				if (map[i][j]=='A'+7) map[i][j]=0;
+				System.out.printf("%3d", dp[i][j]);
 			}
+			System.out.println();
 		}
 	}
-	
-//	private static int dfs (int y, int x) {
-//		if (y<0 || x<0|| y>=N || x>=N) return -1;
-//		if (dp[y][x]!=-1) return dp[y][x];
-//		
-//		//visited[y][x]
-//	}
+
 
 }
