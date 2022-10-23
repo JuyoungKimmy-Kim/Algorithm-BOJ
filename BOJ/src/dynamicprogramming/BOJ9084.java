eec92d28d84eec9d0f8306ce569cc31585981076
Juyoung Kim <kimmykim.jy@gmail.com>
2022-10-23 22:22:55 +0900

    221023

--- BOJ/src/dynamicprogramming/BOJ9084.java
+++ BOJ/src/dynamicprogramming/BOJ9084.java
@@ -6,11 +6,17 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+ * dp[M] : M원을 만들 수 있는 총 가짓수
+ *  
+ * 
+ */
 public class BOJ9084 {
 
 	static final int INF=987654321;
 	static int T,N,M;
 	static int[] coin, dp;
+	
 	public static void main(String[] args) throws IOException  {
 		BufferedReader br=new BufferedReader (new InputStreamReader (System.in));
 		StringTokenizer st=null;
@@ -18,35 +24,27 @@ public class BOJ9084 {
 		T=Integer.parseInt(br.readLine());
 		for (int tc=1; tc<=T; tc++) {
 			N=Integer.parseInt(br.readLine());
-			coin=new int[N];
-			dp=new int[N];
-			Arrays.fill(dp, INF);
+	
+			coin=new int[N+1];			
 			
 			st=new StringTokenizer (br.readLine());
-			for (int i=0; i<N; i++) {
+			for (int i=1; i<=N; i++) {
 				coin[i]=Integer.parseInt(st.nextToken());
-				if (M%coin[i]==0) dp[i]=M/coin[i];
 			}
+
+			M=Integer.parseInt(br.readLine());
+			dp=new int[M+1];
 			
-			for (int i=1; i<N; i++) {
-				for (int j=0; j<i; j++) {
-					if (M%coin[i-coin[j]]==0) {
-						coin[i]+=1;
-					}
+			// 0원을 만드는 방법은 아무것도 선택하지 않는 방법 1가지
+			dp[0]=1;
+			
+			// 자신의 현재 동전 가치보다 작은 금액은 만들 수 없다
+			for (int i=1; i<=N; i++) {
+				for (int j=coin[i]; j<=M; j++) {
+					dp[j]+=dp[j-coin[i]];
 				}
 			}
-			
-			int sum=0;
-			for (int i=0; i<N; i++)
-				sum+=coin[i];
-			
-			System.out.println(sum);
-			
-			
-			
-		}
-		
-		
+			System.out.println(dp[M]);
+		}		
 	}
-
 }
